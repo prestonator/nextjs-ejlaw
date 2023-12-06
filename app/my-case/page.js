2727eb57200f3f9360a00dcc9d18aa3a5b7fd166
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-06 15:48:23 -0600

    Refactor page.js and add mycase.graphql query

--- app/my-case/page.js
+++ app/my-case/page.js
@@ -2,4 +2,31 @@ import { SafeImage, SafeHtml, IconComponent } from "@/utils/helperFunctions";
 import Link from "next/link";
 import styles from "./page.module.css";
 import { fetchData } from "@/lib/fetchData";
-import { HomeQuery } from "@/queries/home.graphql";
+import { MyCaseQuery } from "@/queries/mycase.graphql";
+
+const getData = async () => {
+	const { data } = await fetchData(MyCaseQuery.loc.source.body);
+	return data?.mycase?.data?.attributes;
+};
+
+const MyCasePage = async () => {
+	const { title, slug, hero, sections } = await getData();
+	return (
+		<main>
+			<section>
+				<div>
+					<div>{SafeImage(hero?.image?.data, styles.image)}</div>
+					<button>
+						<Link href={hero?.buttons[0]?.href}>{hero?.buttons[0]?.label}</Link>
+					</button>
+				</div>
+				<div>{SafeHtml(hero?.richText)}</div>
+			</section>
+			<section>
+				<div>{SafeHtml(sections)}</div>
+			</section>
+		</main>
+	);
+};
+
+export default MyCasePage;
