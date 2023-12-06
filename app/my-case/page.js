6bc48977313a9e822da6cd5b91bc2f3e4e4395f0
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-06 15:35:59 -0600

    Add imports and styles for page component

---
+++ app/my-case/page.js
@@ -0,0 +1,5 @@
+import { SafeImage, SafeHtml, IconComponent } from "@/utils/helperFunctions";
+import Link from "next/link";
+import styles from "./page.module.css";
+import { fetchData } from "@/lib/fetchData";
+import { HomeQuery } from "@/queries/home.graphql";
