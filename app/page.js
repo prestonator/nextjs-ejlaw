b46aa5f82c3a28d4375ef0a3df57cd2878d5d749
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-06 14:20:31 -0600

    Add new components and update existing ones

--- app/page.js
+++ app/page.js
@@ -1,12 +1,99 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { SafeImage, SafeHtml, IconComponent } from "@/utils/helperFunctions";
+import Link from "next/link";
+import styles from "./page.module.css";
+import { fetchData } from "@/lib/fetchData";
+import { HomeQuery } from "@/queries/home.graphql";
 
-export default function Home() {
-  return (
-    <main>
-      <div>
-        
-      </div>
-    </main>
-  )
-}
+const getData = async () => {
+	const { data } = await fetchData(HomeQuery.loc.source.body);
+	return data?.home?.data?.attributes;
+};
+
+const Home = async () => {
+	const {
+		title,
+		hero,
+		modal,
+		staffPreviewHeading,
+		staffPreview,
+		whyUsHeading,
+		whyUsCards,
+	} = await getData();
+	return (
+		<main>
+			<section>
+				<div>
+					<div>{SafeHtml(hero.richText)}</div>
+					<div>
+						{hero?.buttons?.map((button) => (
+							<button key={button.id}>
+								<Link href={button.href}>{button.label}</Link>
+							</button>
+						))}
+					</div>
+				</div>
+				<div>{SafeImage(hero.image.data, styles.image)}</div>
+			</section>
+			<section>
+				<div>
+					<div>{SafeHtml(modal?.richText)}</div>
+					<div>
+						<button>
+							<Link href={modal?.buttons[0]?.href}>
+								{modal?.buttons[0]?.label}
+							</Link>
+						</button>
+					</div>
+				</div>
+			</section>
+			<section>
+				<div>{SafeHtml(staffPreviewHeading)}</div>
+				<div>
+					{staffPreview?.map((staff) => (
+						<div key={staff.id}>
+							<div>
+								<div>
+									{staff.socialIcons.map(({ icon, href }) =>
+										IconComponent({
+											icon: icon,
+											customClassName: styles.icon,
+											href: href,
+											key: icon,
+										})
+									)}
+								</div>
+								<div>{SafeImage(staff.image.data, styles.image)}</div>
+							</div>
+							<div>
+								{SafeHtml(staff.richText)}
+								<button>
+									<Link href={staff?.button?.href}>{staff?.button?.label}</Link>
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</section>
+			<section>
+				<div>{SafeHtml(whyUsHeading)}</div>
+				<div>
+					{whyUsCards.map((card) => (
+						<div key={card?.id}>
+							{IconComponent({
+								icon: card?.icon,
+								customClassName: styles.icon,
+								key: card?.id,
+							})}
+							<div>{SafeHtml(card?.richText)}</div>
+							<button>
+								<Link href={card?.button?.href}>{card?.button?.label}</Link>
+							</button>
+						</div>
+					))}
+				</div>
+			</section>
+		</main>
+	);
+};
+
+export default Home;
---
+++ utils/helperFunctions.js
@@ -0,0 +1,40 @@
+import * as ReactIcons from "@/lib/reactIcons";
+import { sanitize } from "isomorphic-dompurify";
+import parse from "html-react-parser";
+import Image from "next/image";
+
+export const SafeImage = (imageData, styles) => {
+	const strapiMediaEndpoint = process.env.STRAPI_MEDIA_ENDPOINT;
+	const url = encodeURI(imageData?.attributes?.url) ?? "";
+	const alt = imageData?.attributes?.alternativeText ?? "";
+
+	return (
+		<Image src={strapiMediaEndpoint + url} alt={alt} width={100} height={100} className={styles} />
+	);
+};
+
+export const SafeHtml = (html) => {
+	const cleanedHtml = sanitize(html);
+	return parse(cleanedHtml);
+};
+
+export const IconComponent = ({ key, icon, href, customClassName }) => {
+	const DynamicIconComponent = ReactIcons[icon];
+
+	if (undefined === DynamicIconComponent) {
+		return null;
+	}
+
+	return href ? (
+		<a
+			key={key}
+			href={href}
+			target="_blank"
+			aria-label="Click to visit the socials for our staff."
+		>
+			<DynamicIconComponent className={customClassName} />
+		</a>
+	) : (
+		<DynamicIconComponent key={icon} className={customClassName} />
+	);
+};
