610a8b22ed00b5bf9c436332d1a954d15afee510
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-07 16:48:23 -0600

    Add OurTeam module and page components

---
+++ app/our-team/[slug]/page.js
@@ -0,0 +1,98 @@
+import {
+	SafeImage,
+	SafeImageUrl,
+	SafeHtml,
+	IconComponent,
+} from "@/utils/helperFunctions";
+import Link from "next/link";
+import styles from "./OurTeam.module.css";
+import { fetchData } from "@/lib/fetchData";
+import { OurTeamsBySlug } from "@/queries/OurTeamBySlug.graphql";
+import { OurTeams } from "@/queries/ourTeam.graphql";
+import TabContainer from "./TabContainer";
+
+const getPage = async (slug) => {
+	try {
+		const { data } = await fetchData(OurTeams.loc.source.body, {
+			filters: {
+				slug: {
+					eq: slug,
+				},
+			},
+		});
+		const { attributes } = data?.ourTeams?.data?.[0] ?? {};
+		return attributes;
+	} catch (error) {
+		console.error("Error fetching team data:", error);
+		return null;
+	}
+};
+
+export async function generateStaticParams() {
+	const { data } = await fetchData(OurTeamsBySlug.loc.source.body);
+	const OurTeamPageData = data?.ourTeams?.data ?? [];
+	return OurTeamPageData.map((page) => ({
+		slug: page.attributes.slug,
+	}));
+}
+
+const Page = async ({ params }) => {
+	const { title, slug, team_member, tabContainer } = await getPage(params.slug);
+
+	const tabs = tabContainer?.tab.map((tab) => ({
+		id: tab.id,
+		title: tab.tabHeading,
+		content: tab.tabContent,
+	}));
+
+	// Create an array of <span> elements with the formatted position names
+	const positionSpans = team_member?.data?.attributes?.positions.map((item) => (
+		<span key={item.id}>{item.text}</span>
+	));
+	return (
+		<main>
+			<section>
+				<div>
+					{SafeImage(team_member.data.attributes.image.data, styles.image)}
+				</div>
+				<div>
+					<h1>{title}</h1>
+					<div>
+						{positionSpans.reduce((positions, position, index) => {
+							positions.push(position);
+							if (index < positionSpans.length - 1) {
+								positions.push(
+									IconComponent({
+										icon: "RxDividerVertical",
+										customClassName: styles.positionDivider,
+									})
+								);
+							}
+							return positions;
+						}, [])}
+					</div>
+					<div>
+						<p className={styles.phone}>
+							<a href={`tel:${team_member.data.attributes.phone}`}>
+								{team_member.data.attributes.phone}
+							</a>
+						</p>
+						<p className={styles.email}>
+							<a href={`mailto:${team_member.data.attributes.email}`}>
+								{team_member.data.attributes.email}
+							</a>
+						</p>
+					</div>
+					<div className={styles.authorBio}>
+						{SafeHtml(team_member.data.attributes.longBio)}
+					</div>
+				</div>
+			</section>
+			<section className={styles.authorTabs}>
+				<TabContainer tabs={tabs} tab={1} />
+			</section>
+		</main>
+	);
+};
+
+export default Page;
