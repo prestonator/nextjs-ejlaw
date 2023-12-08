23bec5485cd4d871265393cd51195293c5e3baab
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-08 11:51:21 -0600

    Add new files and update dependencies

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import { fetchData } from "@/lib/fetchData";
 import { RootLayoutQuery } from "@/queries/rootLayout.graphql";
 import Navbar from "@/components/NavBar/NavBar";
-
+import { cormorant, montserrat, saira } from "@/lib/font";
 
 const getData = async () => {
 	const { data } = await fetchData(RootLayoutQuery.loc.source.body);
@@ -17,7 +17,10 @@ export const metadata = {
 export default async function RootLayout({ children }) {
 	const { navMenu, logo } = await getData();
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			className={`${cormorant.variable} ${montserrat.variable} ${saira.variable}`}
+		>
 			<body>
 				<Navbar navItems={navMenu} logo={logo} />
 				{children}
