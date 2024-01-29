400702769ddd14e2e807734dc718a94cee2fbf2f
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-01-29 15:37:07 -0600

    Update import path for mapImage

--- components/MapPreview/Map.js
+++ components/MapPreview/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import mapImage from "@/public/localMap.webp";
+import mapImage from "../../public/localMap.webp";
 
 const Map = () => {
 	return <Image src={mapImage} alt="Map" className="object-cover" />;
