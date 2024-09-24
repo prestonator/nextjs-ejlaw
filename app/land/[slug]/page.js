ad386a0919738148ac44955937792f81d45db660
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-09-23 19:50:58 -0500

    Add Google Maps integration and contact section to landing page
    - Integrated Google Maps static image in the contact section using the Google Maps API.
    - Added a new contact section with address, phone, and email details.
    - Updated the `reactIcons` library to include `HiOutlineMail` for email icon usage.

--- app/land/[slug]/page.js
+++ app/land/[slug]/page.js
@@ -5,6 +5,7 @@ import {
 	SafeHtml,
 	IconComponent,
 } from "@/utils/helperFunctions";
+import Image from "next/image";
 import { fetchData } from "@/lib/fetchData";
 import { LandingPagesBySlug } from "@/queries/landingPageBySlug.graphql";
 import { LandingPageData } from "@/queries/landingPageData.graphql";
@@ -54,6 +55,8 @@ const Page = async ({ params }) => {
 		section_five_header,
 		staff_cards,
 	} = await getPage(params.slug);
+	const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+	const mapsUrl = `https://maps.googleapis.com/maps/api/staticmap?center=124+E+Main+St,Norman,OK&zoom=15&size=600x300&key=${apiKey}`;
 
 	//console.log(services_card);
 	return (
@@ -277,6 +280,46 @@ const Page = async ({ params }) => {
 								<p className="text-gray-600">{staff.title}</p>
 							</div>
 						))}
+					</div>
+				</div>
+			</section>
+			<section className="bg-gray-100 pt-16 pb-36 px-4 sm:px-6 lg:px-8 animate-slideUp">
+				<div className="max-w-4xl mx-auto text-center">
+					<h2 className="font-fancy text-3xl font-bold text-gray-900 mb-8">
+						Contact Us
+					</h2>
+					<div className="bg-white rounded-lg p-8 mb-8 transition-all duration-300 hover:shadow-lg animate-scaleIn">
+						<h3 className="font-fancy text-xl font-semibold text-gray-900 mb-4">
+							Elton Jenkins Law, P.L.L.C.
+						</h3>
+						<p className="text-gray-700 mb-2">
+							124 E Main Street, Norman, OK 73069
+						</p>
+						<div className="flex justify-center space-x-4 mb-4">
+							<Button variant="link" className="text-primary font-special">
+								{IconComponent({
+									icon: "HiOutlinePhone",
+									customClassName: "mr-2 h-5 w-5",
+								})}
+								(405) 217-3623
+							</Button>
+							<Button variant="link" className="text-primary font-special">
+								{IconComponent({
+									icon: "HiOutlineMail",
+									customClassName: "mr-2 h-5 w-5",
+								})}
+								susan@eltonjenkinslaw.com
+							</Button>
+						</div>
+					</div>
+					<div className="aspect-w-16 aspect-h-9">
+						<Image
+							src={mapsUrl}
+							alt="Map showing the location of Elton Jenkins Law"
+							width={600}
+							height={300}
+							className="rounded-lg shadow-lg object-contain"
+						/>
 					</div>
 				</div>
 			</section>
