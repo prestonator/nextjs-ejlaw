9d67073a80260c63d0caf835ca42f5b2a551efc7
Preston Jenkins <preston@eltonjenkinslaw.com>
2025-02-10 13:48:56 -0600

    refactor: improve NavbarClient styling and layout for better readability

--- components/Navigation/NavClient.jsx
+++ components/Navigation/NavClient.jsx
@@ -26,24 +26,23 @@ const NavbarClient = ({ children }) => {
 	}, []);
 
 	return (
-		<header
-			className={`${styles.header} ${
-				isVisible ? "translate-y-0" : "-translate-y-full"
-			} fixed flex items-center justify-center w-screen h-12 lg:h-20 pt-6 lg:pt-8 xl:pt-12 pb-2 shrink-0 transition-transform duration-300 bg-white`}
-		>
-			<div className="h-[5vh] py-2 text-sm text-center z-4 top-0 bg-white absolute">
-				<a
-					className="text-[#800000] hover:text-blue-700"
-					href="https://elton-jenkins-attorney-at-law.mycase.com/paypage/DNMiVDCbKLCJvWyCSiPEe3FA"
-					target="_blank"
-				>
-					Notice: Visit our payment page to settle your
-					invoices online.
-				</a>
-			</div>
-			{children}
-		</header>
-	);
+    <header
+      className={`${styles.header} ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      } fixed flex items-center justify-center w-screen h-12 lg:h-20 pt-12 lg:pt-16 xl:pt-12 pb-12 shrink-0 transition-transform duration-300 bg-white`}
+    >
+      <div className="h-[5vh] py-2 text-base text-center z-4 top-0 bg-white absolute">
+        <a
+          className="text-[#800000] hover:text-blue-700"
+          href="https://elton-jenkins-attorney-at-law.mycase.com/paypage/DNMiVDCbKLCJvWyCSiPEe3FA"
+          target="_blank"
+        >
+          Notice: Visit our payment page to settle your invoices online.
+        </a>
+      </div>
+      {children}
+    </header>
+  );
 };
 
 export default NavbarClient;
